04bb53b8a4a320db501e52648bad8a078c1bcb6a
akila199910 <akeeumayanga7775@gmail.com>
2024-01-06 10:56:24 +0530

    initialize the spring boot project for backend

---
+++ Back end/hostal/src/main/java/com/hostal/hostal/HostalApplication.java
@@ -0,0 +1,13 @@
+package com.hostal.hostal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalApplication.class, args);
+	}
+
+}
