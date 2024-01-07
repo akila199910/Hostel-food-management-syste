bd6ad63a88a0267476ad9bf607a92b74508f62ff
akila199910 <akeeumayanga7775@gmail.com>
2024-01-07 19:11:47 +0530

    Update Signup.js
    create signup  form

--- Front end/hostal/src/Component/Signup.js
+++ Front end/hostal/src/Component/Signup.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Signup() {
   return (
-    <div>
-      Sign up
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+    <div className="bg-white p-3 rounded w-25">
+      <form action="">
+        <div className="mb-3">
+          <label htmlFor="fname"><strong>First Name</strong></label>
+          <input type="text" placeholder="First Name" className="form-control rounded-0"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lname"><strong>Last Name</strong></label>
+          <input type="text" placeholder="Last Name" className="form-control rounded-0"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="regnumber"><strong>Student Registation Number</strong></label>
+          <input type="text" placeholder="XX/XXXX/XXXXX" className="form-control rounded-0"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="profile"><strong>Profile Picture</strong></label>
+          <input type="file" placeholder="Enter Your Email" className="form-control rounded-0"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email"><strong>Email</strong></label>
+          <input type="email" placeholder="Enter Your Email" className="form-control rounded-0"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password"><strong>Password</strong></label>
+          <input type="password" placeholder="Enter Your Password" className="form-control rounded-0"/>
+        </div>
+        <button className="btn btn-success w-100 rounded-0"><strong>Sign in</strong></button>
+      </form>
     </div>
+  </div>
   )
 }
 export default Signup;
