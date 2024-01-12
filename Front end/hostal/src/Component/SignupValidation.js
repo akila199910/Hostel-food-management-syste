af925c517584d946f94f0781d18e6f028ae7b595
akila199910 <akeeumayanga7775@gmail.com>
2024-01-12 10:03:39 +0530

    create registation form front end

--- Front end/hostal/src/Component/SignupValidation.js
+++ Front end/hostal/src/Component/SignupValidation.js
@@ -1,8 +1,36 @@
-function LoginValidation(value) {
+function SignupValidation(value) {
 
     let error={}
     const email_pattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/
     const password_pattern=/^(?=.*\d)(?=.*[aa-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+    if(value.fname===""){
+        error.fname= "First name should not be empty"
+    }
+    else{
+        error.fname=""
+    }
+
+    if(value.lname===""){
+        error.lname= "Last name should not be empty"
+    }
+    else{
+        error.lname=""
+    }
+
+    if(value.regnumber===""){
+        error.regnumber= "Register  number should not be empty"
+    }
+    else{
+        error.regnumber=""
+    }
+
+    if(value.Picture===""){
+        error.Picture= "Register  number should not be empty"
+    }
+    else{
+        error.Picture=""
+    }
+
     if(value.email===""){
         error.email= "Email should not be empty"
     }
@@ -25,4 +53,4 @@ function LoginValidation(value) {
 
   return error;
 }
-export default LoginValidation;
+export default SignupValidation;
